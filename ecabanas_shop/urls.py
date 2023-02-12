fd1efe9d16e36290d725a79587d392b6830226ba
kikiusen <ecvoracle@gmail.com>
2023-02-12 09:51:12 +0000

    Added accounts, home and media url

--- ecabanas_shop/urls.py
+++ ecabanas_shop/urls.py
@@ -15,14 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include('home.urls')),
+    path('accounts/', include('allauth.urls')),
+    path('', include('home.urls')),
     # path('products/', include('products.urls')),
     # path('cart/', include('cart.urls')),
     # path('checkout/', include('checkout.urls')),
-    path('accounts/', include('allauth.urls')),
     # path('profile/', include('profiles.urls')),
-    path('', include('home.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
