532398f82a7cf608bf72b525d4139a304212f08a
kikiusen <ecvoracle@gmail.com>
2023-03-05 21:18:23 +0000

    Added initial about and faq content

--- home/views.py
+++ home/views.py
@@ -5,4 +5,12 @@ from django.shortcuts import render
 def index(request):
     """ A view to return the index page """
     return render(request, 'home/index.html')
+
+def about_us(request):
+    """ A view to return the about us page """
+    return render(request, 'about_us/about_us.html')
+
+def faq(request):
+    """ A view to return the faq page """
+    return render(request, 'faqs/faqs.html')
     
