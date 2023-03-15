e95a06c41f9a028a8fdca6c540aac821e3c73b65
kikiusen <ecvoracle@gmail.com>
2023-03-15 18:48:45 +0000

    Added toasts, menu/cart processors links and stripe variables

--- ecabanas_shop/settings.py
+++ ecabanas_shop/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+from django.contrib.messages import constants as messages
+import dj_database_url
 
 from os import path
 if path.exists("env.py"):
@@ -25,12 +27,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zc9*2)0ou591d8im#7sii+crq+d$ik0m(bzuh2pl&-ln6%i5i2'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ecabanas-bookstore.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -48,7 +50,23 @@ INSTALLED_APPS = [
     'allauth.socialaccount', # required by allauth
     'home',
     'products',
+    'cart',
+    # 'blog',
+    # 'contact',
 ]
+
+SITE_ID = 1 # required by allauth
+
+LOGIN_REDIRECT_URL = '/' # required by allauth
+LOGOUT_REDIRECT_URL = '/' # required by allauth
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'alert-danger',
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -68,7 +86,10 @@ TEMPLATES = [
         'DIRS': [ 
             os.path.join(BASE_DIR, 'templates'),
             os.path.join(BASE_DIR, 'templates', 'allauth'),
-            os.path.join(BASE_DIR, 'products/templates')],
+            os.path.join(BASE_DIR, 'products/templates'),
+            os.path.join(BASE_DIR, 'cart/templates'),
+            ],
+            
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -77,10 +98,12 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'products.context_processors.book_count_processor', # required by products app
+                'cart.context_processors.cart_contents', # required by cart appq
             ],
         },
     },
 ]
+
 
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
@@ -110,13 +133,18 @@ WSGI_APPLICATION = 'ecabanas_shop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
+if "DATABASE_URL" in os.environ:
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -166,3 +194,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Stripe
+FREE_DELIVERY_THRESHOLD = 25
+STANDARD_DELIVERY_PERCENTAGE = 10
+STRIPE_CURRENCY = 'eur'
