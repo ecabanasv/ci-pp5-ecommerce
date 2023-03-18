20e6afedc5edddb8d8b2867828f8e50c35555cac
kikiusen <ecvoracle@gmail.com>
2023-03-18 23:03:01 +0000

    Added sum of items in orders history lines

---
+++ profiles/templatetags/profile_tools.py
@@ -0,0 +1,10 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def total_quantity(items):
+    total = 0
+    for item in items:
+        total += item.quantity
+    return total
