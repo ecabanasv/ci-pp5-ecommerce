b56bac4b4e5b77c5948a10946863800ea788c7df
kikiusen <ecvoracle@gmail.com>
2023-03-21 14:36:58 +0000

    Added login_required decorator to profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,21 @@
+# shortcuts is used to render templates and get objects or return a 404 error
 from django.shortcuts import render, get_object_or_404
+# messages is used to display messages to the user
 from django.contrib import messages
+# login_required is a decorator that checks if the user is logged in
+from django.contrib.auth.decorators import login_required
 
+# Import the models
 from .models import UserProfile
-from .forms import UserProfileForm
 from checkout.models import Order
 
+# Import the form
+from .forms import UserProfileForm
 
+# profile view
+# @login_required is a decorator that checks if the user is logged in
+
+@login_required
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
@@ -29,7 +39,10 @@ def profile(request):
     return render(request, template, context)
 
 
+# order_history view
+# @login_required is a decorator that checks if the user is logged in
 
+@login_required
 def order_history(request, order_number):
     order = get_object_or_404(Order, order_number=order_number)
 
