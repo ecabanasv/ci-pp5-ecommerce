f77c3feaca67a2d8348b6feffb56b4435c9defa3
kikiusen <ecvoracle@gmail.com>
2023-03-21 14:38:03 +0000

    Added method decortaror for staff member and fix for favourites in details

--- products/views.py
+++ products/views.py
@@ -1,14 +1,30 @@
+# JsonResponse is used to return a JSON response
 from django.http import JsonResponse
+# shortcuts is used to render templates and redirect to URLs
 from django.shortcuts import render, redirect, reverse, get_object_or_404
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.core.paginator import Paginator
-from django.contrib import messages
-from django.db.models import Q
-from django.db.models import Count
+# staff_member_required is a decorator that checks if the user is logged in and is a staff member
+from django.contrib.admin.views.decorators import staff_member_required
+# reverse_lazy is used to reverse a URL to a view when the URL pattern name is not yet available
+from django.urls import reverse_lazy
+# method_decorator is used to apply decorators to class-based views
+from django.utils.decorators import method_decorator
+# login_required is a decorator that checks if the user is logged in
 from django.contrib.auth.decorators import login_required
+# ListView, DetailView, CreateView, UpdateView, and DeleteView are generic class-based views
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+# Paginator is used to paginate the queryset
+from django.core.paginator import Paginator
+# messages is used to display messages to the user
+from django.contrib import messages
+# Q is used to combine multiple queries
+from django.db.models import Q
+# Count is used to count the number of items in a query
+from django.db.models import Count
+# The following imports are used to create a formset
 from .models import Book, FavoriteBook, SubCategory
 
 # The search_books function displays a list of books based on the search term
+
 
 def search_books(request):
     queryset = Book.objects.all()
@@ -27,7 +43,8 @@ def search_books(request):
     books = paginator.get_page(page)
     # Return the search results as a JSON response
     if request.is_ajax():
-        books_data = [{'pk': book.pk, 'title': book.title, 'author': book.author.name, 'isbn': book.isbn, 'cover_image': book.cover_image.url} for book in books]
+        books_data = [{'pk': book.pk, 'title': book.title, 'author': book.author.name,
+                       'isbn': book.isbn, 'cover_image': book.cover_image.url} for book in books]
         return JsonResponse({'books': books_data})
     else:
         return render(request, 'products/book_list.html', {'books': books})
@@ -100,7 +117,8 @@ class BookListView(ListView):
         context['search'] = self.request.GET.get('search', '')
         context['category'] = self.request.GET.get('category', '')
         # Add subcategories with books to the context
-        context['subcategories'] = SubCategory.objects.annotate(book_count=Count('books')).filter(book_count__gt=0).order_by('name')
+        context['subcategories'] = SubCategory.objects.annotate(
+            book_count=Count('books')).filter(book_count__gt=0).order_by('name')
         # Add current subcategory name to the context
         subcategory = self.request.GET.get('subcategory', '')
         context['subcategory'] = subcategory
@@ -116,16 +134,15 @@ class BookListView(ListView):
         context['books'] = books
 
         if self.request.user.is_authenticated:
-            context['favorite_books'] = set(FavoriteBook.objects.filter(user=self.request.user).values_list('book_id', flat=True))
+            context['favorite_books'] = set(FavoriteBook.objects.filter(
+                user=self.request.user).values_list('book_id', flat=True))
         else:
             context['favorite_books'] = set()
 
         return context
 
 
-
 # The BookDetailView displays the details of a specific book
-
 
 class BookDetailView(DetailView):
     # Specify the model to use
@@ -133,9 +150,21 @@ class BookDetailView(DetailView):
     # Specify the template name to use
     template_name = 'products/book_detail.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.user.is_authenticated:
+            context['favorite_books'] = set(FavoriteBook.objects.filter(
+                user=self.request.user).values_list('book_id', flat=True))
+        else:
+            context['favorite_books'] = set()
+
+        return context
+
+
 # The BookCreateView allows the user to create a new book
+# The user must be logged in and a staff member to access this view
 
-
+@method_decorator(login_required, name='dispatch')
 class BookCreateView(CreateView):
     # Specify the model to use
     model = Book
@@ -143,16 +172,22 @@ class BookCreateView(CreateView):
     template_name = 'products/book_form.html'
     # Specify the fields to be included in the form
     fields = ['isbn', 'title', 'small_description', 'description', 'author', 'price',
-              'rating', 'stock', 'cover', 'category', 'subcategory', 'publication_date', 'publisher', 'pages', 'language']
+              'rating', 'stock', 'cover_image', 'category', 'subcategory', 'publication_date', 'publisher', 'pages', 'language']
 
     # Override the form_valid method to set the author field to the currently logged in user
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
+    # staff_member_required is a decorator that checks if the user is a staff member
+    @method_decorator(staff_member_required(login_url=reverse_lazy('home')))
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
 # The BookUpdateView allows the user to update the details of a specific book
+# The user must be logged in and a staff member to access this view
 
-
+@method_decorator(login_required, name='dispatch')
 class BookUpdateView(UpdateView):
     # Specify the model to use
     model = Book
@@ -160,11 +195,17 @@ class BookUpdateView(UpdateView):
     template_name = 'products/book_form.html'
     # Specify the fields to be included in the form
     fields = ['isbn', 'title', 'small_description', 'description', 'author', 'price',
-              'rating', 'stock', 'cover', 'category', 'subcategory', 'publication_date', 'publisher', 'pages', 'language']
+              'rating', 'stock', 'cover_image', 'category', 'subcategory', 'publication_date', 'publisher', 'pages', 'language']
+
+    # staff_member_required is a decorator that checks if the user is a staff member
+    @method_decorator(staff_member_required(login_url=reverse_lazy('home')))
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
 
 # The BookDeleteView allows the user to delete a specific book
+# The user must be logged in and a staff member to access this view
 
-
+@method_decorator(login_required, name='dispatch')
 class BookDeleteView(DeleteView):
     # Specify the model to use
     model = Book
@@ -173,24 +214,44 @@ class BookDeleteView(DeleteView):
     # Specify the URL to redirect to after a successful deletion
     success_url = '/books/'
 
+    # staff_member_required is a decorator that checks if the user is a staff member
+    @method_decorator(staff_member_required(login_url=reverse_lazy('home')))
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
 
-@login_required
+
+
+# add to favorites
+# The user must be logged in to access this view
+
+@method_decorator(login_required)
 def add_to_favorites(request, book_id):
     book = get_object_or_404(Book, id=book_id)
     created = FavoriteBook.objects.get_or_create(user=request.user, book=book)
 
     if created:
-        messages.success(request, f"'{book.title}' has been added to your favorites.", extra_tags='favorites')
+        messages.success(
+            request, f"'{book.title}' has been added to your favorites.", extra_tags='favorites')
     else:
-        messages.warning(request, f"'{book.title}' is already in your favorites.", extra_tags='favorites')
+        messages.warning(
+            request, f"'{book.title}' is already in your favorites.", extra_tags='favorites')
 
-    return redirect('products:book_list')
+    # Redirect back to the previous page
+    return redirect(request.META.get('HTTP_REFERER'))
 
-@login_required
+
+# remove from favorites
+# The user must be logged in to access this view
+
+@method_decorator(login_required)
 def remove_from_favorites(request, book_id):
     book = get_object_or_404(Book, id=book_id)
-    favorite_book = get_object_or_404(FavoriteBook, user=request.user, book=book)
+    favorite_book = get_object_or_404(
+        FavoriteBook, user=request.user, book=book)
     favorite_book.delete()
 
-    messages.success(request, f"'{book.title}' has been removed from your favorites.", extra_tags='favorites')
-    return redirect('products:book_list')
+    messages.success(
+        request, f"'{book.title}' has been removed from your favorites.", extra_tags='favorites')
+
+    # Redirect back to the previous page
+    return redirect(request.META.get('HTTP_REFERER'))
