bd2f153a8c7df1444726f4ba9016c2e944a286e4
kikiusen <ecvoracle@gmail.com>
2023-03-26 17:06:29 +0100

    Updated settings with recaptcha keys

--- ecabanas_shop/settings.py
+++ ecabanas_shop/settings.py
@@ -250,3 +250,8 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+
+# recaptcha
+
+RECAPTCHA_PUBLIC_KEY = os.environ.get('RECAPTCHA_PUBLIC_KEY')
+RECAPTCHA_PRIVATE_KEY = os.environ.get('RECAPTCHA_PRIVATE_KEY')
