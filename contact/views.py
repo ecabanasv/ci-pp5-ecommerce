c01c3b3c6141752e4deae50f76d072321256db1f
kikiusen <ecvoracle@gmail.com>
2023-03-26 15:26:06 +0100

    Contact email functionality POST

--- contact/views.py
+++ contact/views.py
@@ -1,9 +1,17 @@
 # shortcuts is used to render the template
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 # import the ContactForm
 from .forms import ContactForm
 # import messages
 from django.contrib import messages
+# import the send_mail function
+from django.core.mail import send_mail
+# import the View class
+from django.views import generic, View
+# render_to_string is used to render the email template
+from django.template.loader import render_to_string
+# settings is used to get the email settings
+from django.conf import settings
 # import the View class
 from django.views.generic import View
 
@@ -11,33 +19,54 @@ from django.views.generic import View
 
 # Contact View
 class ContactView(View):
-    # Contact Form
-    form = ContactForm
-    # Contact Template
-    template_name = 'contact/contact.html'
-
-    # Get Contact Form
     def get(self, request):
-        # Get the form
-        form = self.form()
-        # Render the template
-        return render(request, self.template_name, {'form': form})
-    
-    # Post Contact Form
+        # get the ContactForm
+        form = ContactForm()
+        # render the template
+        return render(request, 'contact/contact.html', {'form': form})
+
     def post(self, request):
-        # Get the form
-        form = self.form(request.POST)
-        # Check if the form is valid
+        # get the ContactForm
+        form = ContactForm(request.POST)
+        # check if the form is valid
         if form.is_valid():
-            contact = form.save()
-            contact.save()
-            # Send a success message
+            # get the data from the form
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            subject = form.cleaned_data['subject']
+            subject = render_to_string(
+                'contact/confirmation_emails/confirmation_email_subject.txt',
+                {'subject': subject})
+            message = form.cleaned_data['message']
+            message = render_to_string(
+                'contact/confirmation_emails/confirmation_email_body.txt',
+                {'name': name , 'message': message})
+            # send the email
+            send_mail(
+                # subject
+                'Message from ' + name,
+                # message
+                message,
+                # from email
+                email,
+                # subject
+                subject,
+                # to email
+                [settings.EMAIL_HOST_USER],
+                # fail_silently
+                fail_silently=False,
+            )
+            # create a success message
             messages.success(request, 'Your message has been sent!')
-            # Redirect to the home page
-            return render(request, 'home/index.html')
+            # redirect to the contact page
+            return redirect('contact:contact')
+
+        # if the form is not valid
         else:
-            # Send an error message
-            messages.error(request, 'Please correct the error below.')
-            # Redirect to the home page
-            return render(request, 'contact/contact.html', {'form': form})
+            # create an error message
+            messages.error(request, 'Please correct the errors below.')
+            # render the template
+            return render(request, 'contact/contact.html', {'form': form}) 
+    
+
 
