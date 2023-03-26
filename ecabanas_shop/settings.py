58b0fc23f0f62b144821f89d42ef0d7a2be771e3
kikiusen <ecvoracle@gmail.com>
2023-03-26 22:43:19 +0100

    Added error pages and views, also allowed localhost in settings

--- ecabanas_shop/settings.py
+++ ecabanas_shop/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['ecabanas-bookstore.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['ecabanas-bookstore.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
